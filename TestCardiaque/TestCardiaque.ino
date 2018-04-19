3abfc0b90d0ec9f9125d78a47839eca5ee20dccf
andreESGI <andre92.morel@gmail.com>
2018-04-19 18:25:28 +0200

    add temp

--- TestCardiaque/TestCardiaque.ino
+++ TestCardiaque/TestCardiaque.ino
@@ -1,38 +1,38 @@
 /*
- * Copyright (c) 2015, Majenko Technologies
- * All rights reserved.
- * 
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- * 
+   Copyright (c) 2015, Majenko Technologies
+   All rights reserved.
+
+   Redistribution and use in source and binary forms, with or without modification,
+   are permitted provided that the following conditions are met:
+
  * * Redistributions of source code must retain the above copyright notice, this
- *   list of conditions and the following disclaimer.
- * 
+     list of conditions and the following disclaimer.
+
  * * Redistributions in binary form must reproduce the above copyright notice, this
- *   list of conditions and the following disclaimer in the documentation and/or
- *   other materials provided with the distribution.
- * 
+     list of conditions and the following disclaimer in the documentation and/or
+     other materials provided with the distribution.
+
  * * Neither the name of Majenko Technologies nor the names of its
- *   contributors may be used to endorse or promote products derived from
- *   this software without specific prior written permission.
- * 
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
- * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
- * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
- * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
- * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
- * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
- * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+     contributors may be used to endorse or promote products derived from
+     this software without specific prior written permission.
+
+   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+   ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+   WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+   DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR
+   ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+   (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+   LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+   ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+   (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 
 /* Create a WiFi access point and provide a web server on it. */
 #define USE_ARDUINO_INTERRUPTS false
 #include <PulseSensorPlayground.h>
 #include <ESP8266WiFi.h>
-#include <WiFiClient.h> 
+#include <WiFiClient.h>
 #include <ESP8266WebServer.h>
 
 /* Set these to your desired credentials. */
@@ -53,30 +53,30 @@ PulseSensorPlayground pulseSensor;
 
 
 /* Just a little test message.  Go to http://192.168.4.1 in a web browser
- * connected to this access point to see it.
- */
+   connected to this access point to see it.
+*/
 void handleRoot() {
   //String value = String("{\"bpm\":")+BPM+String("}");
-	server.send(200, "application/json", String("{\"bpm\":")+GetBPM()+String("}"));
+  server.send(200, "application/json", String("{\"bpm\":") + GetBPM() + String(", \"temperature\":") + 25 + String("}"));
 }
 
 void setup() {
-  
-	delay(1000);
-	Serial.begin(115200);
+
+  delay(1000);
+  Serial.begin(115200);
   pinMode(buzzer, OUTPUT);
   Serial.println(buzzer);
-	Serial.println();
-	Serial.print("Configuring access point...");
-	/* You can remove the password parameter if you want the AP to be open. */
-	WiFi.softAP(ssid, password);
+  Serial.println();
+  Serial.print("Configuring access point...");
+  /* You can remove the password parameter if you want the AP to be open. */
+  WiFi.softAP(ssid, password);
 
-	IPAddress myIP = WiFi.softAPIP();
-	Serial.print("AP IP address: ");
-	Serial.println(myIP);
-	server.on("/", handleRoot);
-	server.begin();
-	Serial.println("HTTP server started");
+  IPAddress myIP = WiFi.softAPIP();
+  Serial.print("AP IP address: ");
+  Serial.println(myIP);
+  server.on("/", handleRoot);
+  server.begin();
+  Serial.println("HTTP server started");
 
   pulseSensor.analogInput(PIN_INPUT);
   pulseSensor.blinkOnPulse(PIN_BLINK);
@@ -98,30 +98,30 @@ void setup() {
 
        If your Sketch hangs here, try changing USE_PS_INTERRUPT to false.
     */
-    
+
   }
 }
 
-int GetBPM(){
+int GetBPM() {
   int tempval = BPM;
   unsigned long msInit = millis();
-  while(BPM == tempval && (millis()- msInit) < 1500 ){
+  while (BPM == tempval && (millis() - msInit) < 1500 ) {
     if (pulseSensor.sawNewSample()) {
       if (--samplesUntilReport == (byte) 0) {
         samplesUntilReport = SAMPLES_PER_SERIAL_SAMPLE;
         pulseSensor.outputSample();
         Serial.print("BPM: ");
         BPM = pulseSensor.getBeatsPerMinute(0);
-        if(BPM<50 || BPM>200){
+        if (BPM < 50 || BPM > 200) {
           Serial.print("Beep");
           digitalWrite(buzzer, HIGH);
-        }else{
+        } else {
           digitalWrite(buzzer, LOW);
         }
         Serial.print(BPM);
-        
+
         Serial.println(" beats/minutes");
-        
+
         if (pulseSensor.sawStartOfBeat()) {
           pulseSensor.outputBeat();
         }
@@ -133,6 +133,7 @@ int GetBPM(){
 }
 
 void loop() {
-	
+
   server.handleClient();
 }
+
